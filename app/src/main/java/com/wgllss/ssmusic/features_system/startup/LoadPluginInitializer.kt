757f724017f02831c7b685ed85e511fa53d35228
wanggui <wgllss@qq.com>
2022-12-29 22:09:54 +0800

    动态下载 插件

--- app/src/main/java/com/wgllss/ssmusic/features_system/startup/LoadPluginInitializer.kt
+++ app/src/main/java/com/wgllss/ssmusic/features_system/startup/LoadPluginInitializer.kt
@@ -2,37 +2,25 @@ package com.wgllss.ssmusic.features_system.startup
 
 import android.content.Context
 import androidx.startup.Initializer
-import com.wgllss.dynamic.host.library.DynamicDataSourcePluginManagerUser
-import com.wgllss.ssmusic.core.units.DownLoadUtil
-import com.wgllss.ssmusic.core.units.WLog
-import kotlinx.coroutines.*
+import com.wgllss.dynamic.host.library.DynamicPluginHelp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.onEach
-import java.io.File
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
 
 class LoadPluginInitializer : Initializer<Unit> {
 
     override fun create(context: Context) {
-        GlobalScope.launch(Dispatchers.IO) {
-            val url = "https://gitee.com/wgllss888/wgllss-music-data-source/raw/master/jar/classes3_dex.jar"
-            val fileName = url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."))
-            val sb = StringBuilder(context.filesDir.absolutePath)
-                .append(File.separator)
-                .append("dex_s")
-                .append(File.separator)
-                .append(fileName)
-            val file = File(sb.toString())
-            if (file.exists()) {
-                DynamicDataSourcePluginManagerUser.getInstance(context, file.absolutePath)
-                WLog.e(this@LoadPluginInitializer, "文件已经存在:${file.absolutePath}")
-            } else {
-                val file2 = File(file.parent)
-                file2.mkdirs()
-                val downLoadUtil = DownLoadUtil()
-                downLoadUtil.downFile(context, url, file).onEach {
-                    DynamicDataSourcePluginManagerUser.getInstance(context, file.absolutePath)
-                }.collect()
-            }
+        GlobalScope.launch {
+            flow {
+                DynamicPluginHelp().initDynamicPlugin(context)
+                emit(0)
+            }.catch { it.printStackTrace() }
+                .flowOn(Dispatchers.IO)
+                .collect()
         }
     }
 
