5c3afb9360e60e0ecc2aac8555bb9f6b8340f7b5
wgllss <283495283@qq.com>
2023-02-02 15:56:55 +0800

    空初始化方法

--- app/src/main/java/com/wgllss/ssmusic/features_system/startup/LoadPluginInitializer.kt
+++ app/src/main/java/com/wgllss/ssmusic/features_system/startup/LoadPluginInitializer.kt
@@ -2,7 +2,6 @@ package com.wgllss.ssmusic.features_system.startup
 
 import android.content.Context
 import androidx.startup.Initializer
-import com.wgllss.dynamic.host.library.DynamicPluginHelp
 import com.wgllss.core.units.LogTimer
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -12,12 +11,15 @@ class LoadPluginInitializer : Initializer<Unit> {
     override fun create(context: Context) {
         LogTimer.LogE(this, "create")
         GlobalScope.launch {
+            emptyInit(context)
+        }
+    }
+
+    private fun emptyInit(context: Context) {
+        try {
             LogTimer.LogE(this@LoadPluginInitializer, "create ${Thread.currentThread().name}")
-            try {
-                DynamicPluginHelp().initDynamicPlugin(context)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
