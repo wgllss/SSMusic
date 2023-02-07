1700ac204e1633fd4721e99f660a6c38d5321d38
wgllss <283495283@qq.com>
2023-02-07 15:05:06 +0800

    代码布局

--- app/src/main/java/com/wgllss/ssmusic/features_system/startup/InitHomeInitialize.kt
+++ app/src/main/java/com/wgllss/ssmusic/features_system/startup/InitHomeInitialize.kt
@@ -1,12 +1,8 @@
 package com.wgllss.ssmusic.features_system.startup
 
-import android.app.Activity
-import android.app.Application
 import android.content.Context
-import android.os.Bundle
 import androidx.startup.Initializer
 import com.tencent.mmkv.MMKV
-import com.wgllss.ssmusic.core.activity.ActivityManager
 import com.wgllss.ssmusic.core.units.LogTimer
 import com.wgllss.ssmusic.features_third.um.UMHelp
 import kotlinx.coroutines.GlobalScope
@@ -21,38 +17,6 @@ class InitHomeInitialize : Initializer<Unit> {
         GlobalScope.launch {
             MMKV.initialize(context)
             UMHelp.umInit(context)
-            (context.applicationContext as Application).registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
-                override fun onActivityCreated(p0: Activity, p1: Bundle?) {
-                    ActivityManager.instance.pushActivity(p0)
-//                    if (BuildConfig.DEBUG) {
-//                        SMFrameCallback.instance?.start()
-//                        ViewServer.get(p0).addWindow(p0);
-//                    }
-                }
-
-                override fun onActivityStarted(p0: Activity) {
-                }
-
-                override fun onActivityResumed(p0: Activity) {
-//                    if (BuildConfig.DEBUG)
-//                        ViewServer.get(p0).setFocusedWindow(p0);
-                }
-
-                override fun onActivityPaused(p0: Activity) {
-                }
-
-                override fun onActivityStopped(p0: Activity) {
-                }
-
-                override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {
-                }
-
-                override fun onActivityDestroyed(p0: Activity) {
-                    ActivityManager.instance.popActivity(p0)
-//                    if (BuildConfig.DEBUG)
-//                        ViewServer.get(p0).removeWindow(p0);
-                }
-            })
         }
     }
 }
--- app/src/main/java/com/wgllss/ssmusic/features_ui/page/home/fragment/HomeFragment.kt
+++ app/src/main/java/com/wgllss/ssmusic/features_ui/page/home/fragment/HomeFragment.kt
@@ -1,15 +1,23 @@
 package com.wgllss.ssmusic.features_ui.page.home.fragment
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.wgllss.ssmusic.R
 import com.wgllss.ssmusic.core.asyninflater.LaunchInflateKey
 import com.wgllss.ssmusic.core.asyninflater.LayoutContains
+import com.wgllss.ssmusic.core.ex.getIntToDip
 import com.wgllss.ssmusic.core.fragment.BaseViewModelFragment
 import com.wgllss.ssmusic.core.units.LogTimer
+import com.wgllss.ssmusic.core.widget.DividerGridItemDecoration
 import com.wgllss.ssmusic.core.widget.OnRecyclerViewItemClickListener
 import com.wgllss.ssmusic.features_ui.page.home.adapter.MusicAdapter
 import com.wgllss.ssmusic.features_ui.page.home.viewmodels.HomeTabViewModel
@@ -31,9 +39,24 @@ class HomeFragment(val title: String, private val html: String) : BaseViewModelF
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         LogTimer.LogE(this, "$title onCreateView")
-        val view = LayoutContains.getViewByKey(inflater.context, LaunchInflateKey.home_fragment)!!
-        rvPlList = view.findViewById(inflater.context.resources.getIdentifier("rv_pl_list", "id", inflater.context.packageName))
-        return view
+        if (!this::rvPlList.isInitialized) {
+            val res = inflater.context.resources
+            rvPlList = RecyclerView(inflater.context).apply {
+                val lp = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
+                lp.gravity = Gravity.TOP and Gravity.LEFT
+                layoutParams = lp
+                setBackgroundColor(Color.GREEN)
+                layoutManager = LinearLayoutManager(context)
+                val paddingSize = res.getDimension(R.dimen.recycler_padding).toInt()
+                setPadding(paddingSize, 0, paddingSize, 0)
+                val itemDecoration = View(context)
+                val size = context.getIntToDip(1.0f).toInt()
+                itemDecoration.layoutParams = ViewGroup.LayoutParams(size, size)
+                itemDecoration.setBackgroundColor(Color.parseColor("#60000000"))
+                addItemDecoration(DividerGridItemDecoration(context, GridLayoutManager.VERTICAL, itemDecoration))
+            }
+        }
+        return rvPlList
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
