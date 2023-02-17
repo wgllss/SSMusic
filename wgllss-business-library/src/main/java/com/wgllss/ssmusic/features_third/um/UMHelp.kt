590203883dc214f4f0d8a4b522e9e5a09ff2d9b4
wgllss <283495283@qq.com>
2023-02-17 23:50:04 +0800

    um

--- wgllss-business-library/src/main/java/com/wgllss/ssmusic/features_third/um/UMHelp.kt
+++ wgllss-business-library/src/main/java/com/wgllss/ssmusic/features_third/um/UMHelp.kt
@@ -1,12 +1,11 @@
 package com.wgllss.ssmusic.features_third.um
-//
 import android.content.Context
-//import com.umeng.commonsdk.UMConfigure
-//import com.wgllss.ssmusic.BuildConfig
-//import com.wgllss.ssmusic.features_system.savestatus.MMKVHelp
-//import kotlinx.coroutines.Dispatchers
-//import kotlinx.coroutines.GlobalScope
-//import kotlinx.coroutines.launch
+import com.umeng.commonsdk.UMConfigure
+import com.wgllss.ssmusic.features_system.savestatus.MMKVHelp
+import com.wgllss.ssmusic.library.BuildConfig
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 object UMHelp {
     const val APP_KEY = "634d040505844627b5672f2f"
@@ -15,21 +14,21 @@ object UMHelp {
 
     fun umInit(context: Context) {
 //        GlobalScope.launch(Dispatchers.IO) {
-//            try {
-//                UMConfigure.preInit(context, APP_KEY, "umeng");
-//                UMConfigure.setLogEnabled(BuildConfig.DEBUG)
-//                if (MMKVHelp.getUmInit() == 1) {
-//                    UMConfigure.init(context, APP_KEY, "umeng", UMConfigure.DEVICE_TYPE_PHONE, UMENG_MESSAGE_SECRET)
-//                } else {
-//                    MMKVHelp.setUmInit()
-//                    UMConfigure.submitPolicyGrantResult(context, true)
-//                    /*** 友盟sdk正式初始化*/
-//                    /*** 友盟sdk正式初始化 */
-//                    UMConfigure.init(context, APP_KEY, "umeng", UMConfigure.DEVICE_TYPE_PHONE, UMENG_MESSAGE_SECRET)
-//                }
-//            } catch (e: Exception) {
-//                e.printStackTrace()
-//            }
+        try {
+            UMConfigure.preInit(context, APP_KEY, "umeng");
+            UMConfigure.setLogEnabled(BuildConfig.DEBUG)
+            if (MMKVHelp.getUmInit() == 1) {
+                UMConfigure.init(context, APP_KEY, "umeng", UMConfigure.DEVICE_TYPE_PHONE, UMENG_MESSAGE_SECRET)
+            } else {
+                MMKVHelp.setUmInit()
+                UMConfigure.submitPolicyGrantResult(context, true)
+                /*** 友盟sdk正式初始化*/
+                /*** 友盟sdk正式初始化 */
+                UMConfigure.init(context, APP_KEY, "umeng", UMConfigure.DEVICE_TYPE_PHONE, UMENG_MESSAGE_SECRET)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
 //        }
     }
 }
