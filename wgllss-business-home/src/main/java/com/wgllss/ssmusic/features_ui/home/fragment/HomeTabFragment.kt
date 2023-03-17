7944b0316838e885e0ef8d0047c556ca196c0d11
wgllss <283495283@qq.com>
2023-03-17 11:47:14 +0800

    懒加载 attach

--- wgllss-business-home/src/main/java/com/wgllss/ssmusic/features_ui/home/fragment/HomeTabFragment.kt
+++ wgllss-business-home/src/main/java/com/wgllss/ssmusic/features_ui/home/fragment/HomeTabFragment.kt
@@ -43,18 +43,18 @@ class HomeTabFragment : BaseViewModelFragment<HomeViewModel>(0) {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        childAdapter = ViewPage2ChildFragmentAdapter(childFragmentManager, lifecycle)
-        childAdapter.notifyData(getList())
-        mTabLayoutMediator = TabLayoutMediator(homeTabLayout, viewPager2) { tab: TabLayout.Tab, position: Int ->
-            val textView = TextView(requireContext())
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18f)
-            textView.setTextColor(ThemeUtils.getColorOnPrimary(requireContext()))
-            tab.customView = textView
-            textView.text = (childAdapter.list[position] as HomeFragment).title
-        }
-//            .apply(TabLayoutMediator::attach)
-        viewPager2.adapter = childAdapter
         viewModel.lazyTabViewPager2.observe(viewLifecycleOwner) {
+            childAdapter = ViewPage2ChildFragmentAdapter(childFragmentManager, lifecycle)
+            childAdapter.notifyData(getList())
+            mTabLayoutMediator = TabLayoutMediator(homeTabLayout, viewPager2) { tab: TabLayout.Tab, position: Int ->
+                val textView = TextView(requireContext())
+                textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18f)
+                textView.setTextColor(ThemeUtils.getColorOnPrimary(requireContext()))
+                tab.customView = textView
+                textView.text = (childAdapter.list[position] as HomeFragment).title
+            }
+//            .apply(TabLayoutMediator::attach)
+            viewPager2.adapter = childAdapter
             mTabLayoutMediator?.attach()
             homeTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
                 override fun onTabSelected(tab: TabLayout.Tab) {
