0a694286627f62b7edf4cb38790184f59f06ef05
wgllss <283495283@qq.com>
2023-09-01 13:47:02 +0800

    TAB 框架代码整理

--- app/src/main/java/com/wgllss/ssmusic/features_ui/page/classics/fragment/HomeTabFragment.kt
+++ app/src/main/java/com/wgllss/ssmusic/features_ui/page/classics/fragment/HomeTabFragment.kt
@@ -1,104 +1,16 @@
 package com.wgllss.ssmusic.features_ui.page.classics.fragment
 
-import android.os.Bundle
-import android.util.TypedValue
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
-import com.wgllss.core.adapter.ViewPage2ChildFragmentAdapter
-import com.wgllss.core.fragment.BaseViewModelFragment
-import com.wgllss.core.material.ThemeUtils
-import com.wgllss.core.units.LogTimer
-import com.wgllss.ssmusic.features_system.startup.HomeContains
-import com.wgllss.ssmusic.features_system.startup.LaunchInflateKey
+import com.wgllss.ssmusic.features_ui.home.fragment.BaseTabFragment
 import com.wgllss.ssmusic.features_ui.home.fragment.TabTitleFragment
-import com.wgllss.ssmusic.features_ui.page.home.viewmodels.HomeViewModel2
+import com.wgllss.ssmusic.features_ui.home.viewmodels.HomeViewModel
 
 
-class HomeTabFragment : BaseViewModelFragment<HomeViewModel2>(0) {
+class HomeTabFragment : BaseTabFragment<HomeViewModel>() {
 
-    private lateinit var childAdapter: ViewPage2ChildFragmentAdapter
-    private lateinit var homeTabLayout: TabLayout
-    private lateinit var viewPager2: ViewPager2
-    private var mTabLayoutMediator: TabLayoutMediator? = null
+    override fun isLazyTab() = false
 
-    override fun activitySameViewModel() = true
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        LogTimer.LogE(this, "onCreateView")
-        val view = HomeContains.getViewByKey(inflater.context, LaunchInflateKey.home_tab_fragment_layout)!!
-        homeTabLayout = view.findViewById(inflater.context.resources.getIdentifier("homeTabLayout", "id", inflater.context.packageName))
-        viewPager2 = view.findViewById(inflater.context.resources.getIdentifier("homeViewPager2", "id", inflater.context.packageName))
-        return view
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        LogTimer.LogE(this, "onViewCreated")
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-//        viewModel.lazyTabViewPager2.observe(viewLifecycleOwner) {
-        childAdapter = ViewPage2ChildFragmentAdapter(childFragmentManager, lifecycle)
-        childAdapter.notifyData(getList())
-        mTabLayoutMediator = TabLayoutMediator(homeTabLayout, viewPager2) { tab: TabLayout.Tab, position: Int ->
-            val textView = TextView(requireContext())
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18f)
-            textView.setTextColor(ThemeUtils.getColorOnPrimary(requireContext()))
-            tab.customView = textView
-            textView.text = (childAdapter.list[position] as TabTitleFragment<*>).title
-        }
-//            .apply(TabLayoutMediator::attach)
-        viewPager2.adapter = childAdapter
-        mTabLayoutMediator?.attach()
-        homeTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                tab?.customView?.takeIf {
-                    it is TextView
-                }?.run {
-                    (this as TextView).run {
-                        setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20f)
-//                            setTextColor(resources.getColor(R.color.colorPrimary))
-                    }
-                }
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab) {
-                tab?.customView?.takeIf {
-                    it is TextView
-                }?.run {
-                    (this as TextView).run {
-                        setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18f)
-//                            setTextColor(resources.getColor(R.color.white))
-                    }
-                }
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab) {
-            }
-        })
-        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                if (position > 0 && viewPager2.offscreenPageLimit != childAdapter.itemCount)
-                    viewPager2.offscreenPageLimit = childAdapter.itemCount
-            }
-        })
-//        }
-        LogTimer.LogE(this, "onActivityCreated")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        LogTimer.LogE(this, "onResume")
-    }
-
-    private fun getList() = mutableListOf<Fragment>(
+    override fun getList() = mutableListOf<Fragment>(
         TabTitleFragment.newInstance("推荐", "index", HomeFragment::class.java),
         TabTitleFragment.newInstance("华语", "forum-1", HomeFragment::class.java),
         TabTitleFragment.newInstance("日韩", "forum-15", HomeFragment::class.java),
@@ -107,9 +19,4 @@ class HomeTabFragment : BaseViewModelFragment<HomeViewModel2>(0) {
         TabTitleFragment.newInstance("纯音乐", "forum-12", HomeFragment::class.java),
         TabTitleFragment.newInstance("异次元", "forum-13", HomeFragment::class.java),
     )
-
-    override fun onDetach() {
-        super.onDetach()
-        mTabLayoutMediator?.detach()
-    }
 }
