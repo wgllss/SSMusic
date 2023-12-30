f68b1fa955e05e9b4331dbccc07957cb480e95d2
wgllss <283495283@qq.com>
2023-12-30 23:14:26 +0800

    MV 播放UI 修改

--- app/src/main/java/com/wgllss/ssmusic/features_ui/page/home/adapter/MVListAdapter.kt
+++ app/src/main/java/com/wgllss/ssmusic/features_ui/page/home/adapter/MVListAdapter.kt
@@ -2,13 +2,11 @@ package com.wgllss.ssmusic.features_ui.page.home.adapter
 
 
 import android.content.Context
+import android.graphics.Color
 import android.util.TypedValue
 import android.view.Gravity
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.imageview.ShapeableImageView
 import com.google.android.material.shape.RoundedCornerTreatment
@@ -22,6 +20,7 @@ import com.wgllss.ssmusic.datasource.netbean.mv.KMVItem
 class MVListAdapter : BaseRecyclerAdapter<KMVItem>() {
     private val img = 1
     private val titleName = 2
+    private val play = 3
     private val footer by lazy { KMVItem("", "", "", 1) }
 
     override fun getLayoutResId(viewType: Int): Int {
@@ -45,7 +44,7 @@ class MVListAdapter : BaseRecyclerAdapter<KMVItem>() {
             context = parent.context
         }
         if (viewType == 0) {
-            val linearLayout = LinearLayout(context).apply {
+            val frameLayout = FrameLayout(parent.context).apply {
                 layoutParams = RecyclerView.LayoutParams(RecyclerView.LayoutParams.MATCH_PARENT, context.getIntToDip(170f).toInt())
                 val array: IntArray = intArrayOf(android.R.attr.selectableItemBackground)
                 val typedValue = TypedValue()
@@ -54,33 +53,45 @@ class MVListAdapter : BaseRecyclerAdapter<KMVItem>() {
                 attr.recycle()
                 isClickable = true
                 isFocusable = true
-                gravity = Gravity.CENTER_HORIZONTAL
-                orientation = LinearLayout.VERTICAL
+//                gravity = Gravity.CENTER_HORIZONTAL
+//                orientation = LinearLayout.VERTICAL
                 val size = context.getIntToDip(6f).toInt()
                 setPadding(size, size, size, size)
             }
+            val imgHeight = parent.context.getIntToDip(120f).toInt()
             val image = ShapeableImageView(parent.context).apply {
                 id = img
-                val size = context.getIntToDip(120f).toInt()
                 scaleType = ImageView.ScaleType.FIT_XY
-                val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, size)
+                val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, imgHeight)
                 layoutParams = lp
                 shapeAppearanceModel = ShapeAppearanceModel.builder().apply {
                     setAllCorners(RoundedCornerTreatment())
                     setAllCornerSizes(context.getIntToDip(8f)) //设置圆， 40为正方形边长 80 一半，等于半径 ，需要注意单位
                 }.build()
             }
-            linearLayout.addView(image)
+            frameLayout.addView(image)
             val textViewMusicName = TextView(context).apply {
                 id = titleName
                 val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
                 lp.gravity = Gravity.CENTER_HORIZONTAL
+                lp.topMargin = imgHeight
                 maxLines = 2
                 setTextSize(TypedValue.COMPLEX_UNIT_DIP, 16f)
                 layoutParams = lp
             }
-            linearLayout.addView(textViewMusicName)
-            return BaseBindingViewHolder(linearLayout)
+            frameLayout.addView(textViewMusicName)
+            val imgPlay = ImageView(parent.context).apply {
+                id = play
+                val size = context.getIntToDip(45f).toInt()
+                layoutParams = FrameLayout.LayoutParams(size, size).apply {
+                    gravity = Gravity.CENTER_HORIZONTAL
+                    topMargin = imgHeight / 2 - size / 2
+                }
+//                val padding = context.getIntToDip(3f).toInt()
+//                setPadding(padding, padding, padding, padding)
+            }
+            frameLayout.addView(imgPlay)
+            return BaseBindingViewHolder(frameLayout)
         } else {
             return BaseBindingViewHolder(ProgressBar(context).apply {
                 layoutParams = RecyclerView.LayoutParams(RecyclerView.LayoutParams.MATCH_PARENT, RecyclerView.LayoutParams.WRAP_CONTENT).apply {
@@ -94,6 +105,11 @@ class MVListAdapter : BaseRecyclerAdapter<KMVItem>() {
         if (getItemViewType(position) == 0) {
             holder.itemView.findViewById<TextView>(titleName).text = item.title
             holder.itemView.findViewById<ShapeableImageView>(img).loadUrl(item.imgUrl)
+            val res = context.resources
+            holder.itemView.findViewById<ImageView>(play).apply {
+                setImageDrawable(res.getDrawable(res.getIdentifier("play_btn_play_selector", "drawable", context.packageName)))
+//                background = res.getDrawable(res.getIdentifier("oval_play", "drawable", context.packageName))
+            }
         }
     }
 }
