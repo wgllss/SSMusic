009ac1fa0456a52927b84e77ea5f180b81c2fe0f
wgllss <283495283@qq.com>
2024-01-03 22:17:11 +0800

    新歌 经典 UI 调整

--- app/src/main/java/com/wgllss/ssmusic/features_ui/page/classics/fragment/HomeTabFragment.kt
+++ app/src/main/java/com/wgllss/ssmusic/features_ui/page/classics/fragment/HomeTabFragment.kt
@@ -1,6 +1,14 @@
 package com.wgllss.ssmusic.features_ui.page.classics.fragment
 
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.wgllss.core.ex.finishActivity
+import com.wgllss.ssmusic.R
 import com.wgllss.ssmusic.features_ui.home.fragment.BaseTabFragment
 import com.wgllss.ssmusic.features_ui.home.fragment.TabTitleFragment
 import com.wgllss.ssmusic.features_ui.home.viewmodels.HomeViewModel
@@ -8,7 +16,33 @@ import com.wgllss.ssmusic.features_ui.home.viewmodels.HomeViewModel
 
 class HomeTabFragment : BaseTabFragment<HomeViewModel>() {
 
+
+    private lateinit var imgBack: ImageView
+    private lateinit var root: View
     override fun isLazyTab() = false
+
+    override fun getTextColor(): Int {
+        val typedValue = TypedValue()
+        requireActivity().theme.resolveAttribute(android.R.attr.colorPrimary, typedValue, true)
+        return typedValue.data
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        if (!this::root.isInitialized) {
+            root = inflater.inflate(R.layout.fragment_classes_songsheet, container, false)
+            homeTabLayout = root.findViewById(inflater.context.resources.getIdentifier("tab_view", "id", inflater.context.packageName))
+            viewPager2 = root.findViewById(inflater.context.resources.getIdentifier("homeViewPager2", "id", inflater.context.packageName))
+            imgBack = root.findViewById(R.id.img_back)
+        }
+        return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        imgBack.setOnClickListener {
+            activity?.run { finishActivity() }
+        }
+    }
 
     override fun getList() = mutableListOf<Fragment>(
         TabTitleFragment.newInstance("推荐", "index", HomeFragment::class.java),
